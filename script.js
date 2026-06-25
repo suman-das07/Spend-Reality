d5ac87a392a6e6bb2de98bb786a413ab8a88a11f
SUMAN DAS <sumanda0708@gmail.com>
2026-06-25 17:05:26 +0530

    Designing phase completed with HTML & CSS.

---
+++ script.js
@@ -0,0 +1,17 @@
+let price = Number(document.querySelector("#price").value);
+let hourWage = Number(document.querySelector("#hourWage").value);
+let itemName = document.querySelector("#itemName").value;
+let monthlySalary = Number(document.querySelector("#monthlySalary").value);
+let report= document.querySelector(".reportModal")
+
+let generateReportbtn = document.querySelector("#generateReport")
+
+generateReportbtn.addEventListener("click", function(){
+  let price = Number(document.querySelector("#price").value);
+  let hourWage = Number(document.querySelector("#hourWage").value);
+  let itemName = document.querySelector("#itemName").value;
+  let monthlySalary = Number(document.querySelector("#monthlySalary").value);
+  let report = document.querySelector(".reportModal")
+
+  let generateReportbtn = document.querySelector("#generateReport")
+});
